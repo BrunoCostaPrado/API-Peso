f3074066e8d9b65405ff585168acc5933e1a9f5f
BrunoCostaPrado <202203973541@alunos.estacio.br>
2024-05-23 00:08:32 -0300

    add get/set Weigh

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { app, host, port } from "$lib/api"
 
+import { getWeigh } from "$routes/get-weigh"
 import { hello } from "$routes/hello"
 import { hello_ws } from "$routes/hello-ws"
 import { root } from "$routes/root"
+import { setWeigh } from "$routes/set-weigh"
 import websocket from "@fastify/websocket"
 
 app.register(websocket, { options: { maxPayload: 1024 } })
 app.register(hello)
 app.register(hello_ws)
 app.register(root)
+app.register(setWeigh)
+app.register(getWeigh)
 
 export async function main() {
 	await app.listen({ port, host })
--- src/routes/set-weigh.ts
+++ src/routes/set-weigh.ts
@@ -1,11 +1,22 @@
 import { app } from "../lib/api"
-import { weigh, Weigh } from "../lib/types"
+import { type weigh, Weigh as Peso } from "../lib/types"
+import { Weigh } from "../drizzle/schema"
+import { db } from "../drizzle/db"
 
-export async function weigh() {
+export async function setWeigh() {
 	app.post("/setWeigh", async (req, reply) => {
-		const { date, weigh } = Weigh.parse(req.body)
-		// const setWeigh = await weigh.create({})
-
-		return reply.status(201).send({})
+		console.time("set Weigh")
+		const { date, weigh } = Peso.parse(req.body)
+		const push = await db
+			.insert(Weigh)
+			.values({
+				date: date,
+				weigh: weigh
+			})
+			.returning({
+				id: Weigh.id
+			})
+		console.timeEnd("set Weigh")
+		return reply.status(201).send({ push })
 	})
 }
